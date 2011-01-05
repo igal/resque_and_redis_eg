1d81735aadf8916ec7247008a16e302f60e1b6cd
Igal Koshevoy <igal@pragmaticraft.com>
2011-01-04 18:09:32 -0800

    Added sample output to redis_eg.

--- lib/redis_eg.rb
+++ lib/redis_eg.rb
@@ -10,15 +10,19 @@ class RedisEg
     print "* Set key 'mystring' to value 'myvalue': "
     redis.set :mystring, 'myvalue'
     puts redis.get :mystring
+    # => myvalue
 
     print "* Type of key 'mystring': "
     puts redis.type :mystring
+    # => string
 
     print "* Exists key 'mystring': "
     puts redis.exists :mystring
+    # => true
 
     print "* Delete key 'mystring': "
     puts redis.del :mystring
+    # => 1
 
     puts '#---[ Lists ]-----------------------------------------------------------'
 
@@ -28,12 +32,15 @@ class RedisEg
     redis.rpush :mylist, 'b'
     redis.rpush :mylist, 'c'
     p redis.lrange(:mylist, 0, -1)
+    # => ["a", "b", "c"]
 
     print "* Pop item from list: "
     p redis.lpop :mylist
+    # => "a"
 
     print "* Length of list: "
     p redis.llen :mylist
+    # => 2
 
     puts '#---[ Sets ]------------------------------------------------------------'
 
@@ -42,12 +49,16 @@ class RedisEg
     redis.sadd :myset, 'apple'
     redis.sadd :myset, 'banana'
     redis.sadd :myset, 'cherry'
+    p redis.smembers :myset
+    # => ["cherry", "banana", "apple"]
 
     print "* Item 'cherry' exists in set: "
     p redis.sismember :myset, 'cherry'
+    # => true
 
     print "* Insert duplicate item into set: "
     p redis.sadd :myset, 'cherry'
+    # => false
 
     puts '#---[ Sorted sets ]-----------------------------------------------------'
 
@@ -57,21 +68,25 @@ class RedisEg
     redis.zadd :myzset, 10, 'ten'
     redis.zadd :myzset, 20, 'twenty'
     p redis.zrange :myzset, 0, -1
+    # => ["ten", "twenty", "thirty"]
 
     print "* Items with scores from 0 to 20: "
     p redis.zrangebyscore :myzset, 0, 20
+    # => ["ten", "twenty"]
 
     puts '#---[ Hash ]------------------------------------------------------------'
 
     print "* Add hash item: "
     redis.del :myhash
     redis.mapped_hmset 'myhash', {
-      :name => 'Benjamin Willard', 
-      :rank => 'Captain'
+      :name => 'Portland Ruby Brigade', 
+      :abbreviation => 'pdxruby'
     }
     p redis.hgetall :myhash
+    # => {"name"=>"Portland Ruby Brigade", "abbreviation"=>"pdxruby"}
 
     print "* Get field for item: "
-    puts redis.hget :myhash, 'name'
+    puts redis.hget :myhash, 'abbreviation'
+    # => pdxruby
   end
 end
